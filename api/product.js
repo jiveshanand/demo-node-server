815ef86fc188658ac803ac7aa7065b8eb3ed54b6
Your Name <you@example.com>
2022-08-25 19:39:21 +0530

    initial commit

---
+++ api/product.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const convert = require('xml-js');
+
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
+router.get('/', async (req, response) => {
+  try {
+    axios
+      .get('https://www.nasdaq.com/feed/rssoutbound?category=Technology')
+      .then((res) => {
+        console.log(res.data);
+        const convertedResponse = convert.xml2js(res.data);
+        response.json(convertedResponse);
+      });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
